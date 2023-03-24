b87a976591805a73ab8eabe74a3c8fb578041e81
Agnaldo <agnaldoozorio@hotmail.com>
2023-03-24 14:38:51 -0300

    first commit

---
+++ src/br/dio/com/PrineiroPrograma.java
@@ -0,0 +1,13 @@
+package br.dio.com;
+
+import br.dio.com.model.Gato;
+
+public class PrineiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = news();
+
+      /*  int a = 5;
+        int b = 3;
+        System.out.println("hello word " + (a+b));*/
+    }
+}
